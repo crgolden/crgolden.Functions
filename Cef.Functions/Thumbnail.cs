87b1edd7ecdb50e3a26fe39df7aa493b7a2d2028
Chris Golden <crgolden@msn.com>
2019-01-01 16:13:16 -0600

    Initial commit

---
+++ Cef.Functions/Thumbnail.cs
@@ -0,0 +1,111 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+// Learn how to locally debug an Event Grid-triggered function:
+//    https://aka.ms/AA30pjh
+
+// Use for local testing:
+//   https://{ID}.ngrok.io/runtime/webhooks/EventGrid?functionName=Thumbnail
+
+namespace Cef.Functions
+{
+    using System;
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.EventGrid.Models;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using Newtonsoft.Json.Linq;
+    using SixLabors.ImageSharp;
+    using SixLabors.ImageSharp.Formats;
+    using SixLabors.ImageSharp.Formats.Gif;
+    using SixLabors.ImageSharp.Formats.Jpeg;
+    using SixLabors.ImageSharp.Formats.Png;
+    using SixLabors.ImageSharp.Processing;
+
+    public static class Thunbnail
+    {
+        private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        private static string GetBlobNameFromUrl(string blobUri)
+        {
+            var uri = new Uri(blobUri);
+            var cloudBlob = new CloudBlob(uri);
+            return cloudBlob.Name;
+        }
+
+        private static IImageEncoder GetEncoder(string extension)
+        {
+            if (!Regex.IsMatch(
+                input: extension.Replace(".", ""),
+                pattern: "gif|png|jpe?g",
+                options: RegexOptions.IgnoreCase)) return null;
+            switch (extension)
+            {
+                case "png":
+                    return new PngEncoder();
+                case "jpg":
+                    return new JpegEncoder();
+                case "jpeg":
+                    return new JpegEncoder();
+                case "gif":
+                    return new GifEncoder();
+                default:
+                    return null;
+            }
+        }
+
+        [FunctionName("Thumbnail")]
+        public static async Task Run(
+            [EventGridTrigger]EventGridEvent eventGridEvent,
+            [Blob("{data.url}", FileAccess.Read)] Stream input,
+            ILogger log)
+        {
+            try
+            {
+                if (input != null)
+                {
+                    var createdEvent = ((JObject)eventGridEvent.Data).ToObject<StorageBlobCreatedEventData>();
+                    var extension = Path.GetExtension(createdEvent.Url);
+                    var encoder = GetEncoder(extension);
+
+                    if (encoder != null)
+                    {
+                        var thumbnailWidth = Convert.ToInt32(Environment.GetEnvironmentVariable("THUMBNAIL_WIDTH"));
+                        var thumbContainerName = Environment.GetEnvironmentVariable("THUMBNAIL_CONTAINER_NAME");
+                        var storageAccount = CloudStorageAccount.Parse(BLOB_STORAGE_CONNECTION_STRING);
+                        var blobClient = storageAccount.CreateCloudBlobClient();
+                        var container = blobClient.GetContainerReference(thumbContainerName);
+                        var blobName = GetBlobNameFromUrl(createdEvent.Url);
+                        var blockBlob = container.GetBlockBlobReference(blobName);
+
+                        using (var output = new MemoryStream())
+                        using (var image = Image.Load(input))
+                        {
+                            var divisor = image.Width / thumbnailWidth;
+                            var height = Convert.ToInt32(Math.Round((decimal)(image.Height / divisor)));
+
+                            image.Mutate(x => x.Resize(thumbnailWidth, height));
+                            image.Save(output, encoder);
+                            output.Position = 0;
+                            await blockBlob.UploadFromStreamAsync(output);
+                        }
+                    }
+                    else
+                    {
+                        log.LogInformation($"No encoder support for: {createdEvent.Url}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+                throw;
+            }
+        }
+    }
+}
